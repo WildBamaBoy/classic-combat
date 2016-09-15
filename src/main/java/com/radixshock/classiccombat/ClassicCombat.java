0c6f39ff07e270c99ec94e1ba416333580360e90
WildBamaBoy <wildbamaboy@gmail.com>
2016-09-14 20:19:50 -0500

    Commit total work

---
+++ src/main/java/com/radixshock/classiccombat/ClassicCombat.java
@@ -0,0 +1,39 @@
+package com.radixshock.classiccombat;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ClassicCombat.MODID, version = ClassicCombat.VERSION)
+public class ClassicCombat
+{
+	@Instance(ClassicCombat.MODID)
+	private static ClassicCombat instance;
+	
+    public static final String MODID = "classiccombat";
+    public static final String VERSION = "1.0.0";
+    private Logger log = null;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	instance = this;
+    	log = event.getModLog();
+
+    	MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
+    }
+    
+    public Logger getLog()
+    {
+    	return log;
+    }
+    
+    public static ClassicCombat getInstance()
+    {
+    	return instance;
+    }
+}
---
+++ src/main/java/com/radixshock/classiccombat/ForgeEventHandler.java
@@ -0,0 +1,49 @@
+package com.radixshock.classiccombat;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ForgeEventHandler 
+{
+	/** Grab the field used for the swing tick cooldown instead of looking it up for each player. It's all the same. */
+	private static Field targetField = EntityLivingBase.class.getDeclaredFields()[22];
+	
+	static
+	{
+		//Don't waste time setting accessibility each time. Not required.
+		targetField.setAccessible(true);
+	}
+	
+	/**
+	 * When a player updates, their swing tick cooldown will be set to 20 if it is less than 20.
+	 * This effectively removes the cooldown aspect of gameplay, reverting the combat system
+	 * back to a pseudo-classic feel.
+	 * 
+	 * Very very fast, measured to be completed in about 0.005 milliseconds.
+	 */
+	@SubscribeEvent
+	public void handleLivingUpdate(LivingUpdateEvent event)
+	{
+		try
+		{
+			if (event.getEntity() instanceof EntityPlayerMP)
+			{
+				EntityPlayerMP player = (EntityPlayerMP)event.getEntity();
+
+				if (targetField.getInt(player) < 20)
+				{
+					targetField.set(player, 20);
+				}
+			}
+		}
+		
+		catch (Exception e)
+		{
+			//Can't foresee any exceptions, so just throw them out.
+		}
+	}
+}
